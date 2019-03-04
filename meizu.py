ef9e49307e96000e14519781fcfe9ef351110555
rahmadxyz <48191546+rahmadxyz@users.noreply.github.com>
2019-03-04 15:06:53 +0700

    Update meizu.py

--- meizu.py
+++ meizu.py
@@ -2,7 +2,7 @@ import os, sys
 
 print ("\033[1;32mSilahkan Masukkan Username & Password Anda")
 
-print ("\033[1;32matau silahkan Hubungi Mr.Edi ")
+print ("\033[1;32matau silahkan Hubungi MRR ")
 
 username = 'Rahmad'      
 
